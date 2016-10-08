126c6689d335e6b2db5c528bc9b1e4f1bdba78d7
Jennifer Currie <renestl@gmail.com>
2016-10-08 02:01:37 -0500

    added basic portfolio structure

---
+++ js/ia-reverse.js
@@ -0,0 +1,30 @@
+// Code goes here
+
+ $(document).ready(function() {
+  
+  
+  
+  
+  
+  
+  
+  
+ // reverses order of divs so site appears to flow backwards 
+  // var reverse = $('#content')[0],
+  //     divs = reverse.children,
+  //     order = divs.length - 1;
+      
+  // for (order; order > 0; order--) {
+  //   reverse.appendChild(divs[order]);
+  // }
+  
+});
+
+
+// var parent = document.getElementsByClassName('top-to-bottom')[0],
+//     divs = parent.children,
+//     i = divs.length - 1;
+
+// for (; i--;) {
+//     parent.appendChild(divs[i])
+// }
